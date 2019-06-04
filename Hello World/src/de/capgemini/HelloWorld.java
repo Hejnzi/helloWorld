652f62794d4fd7786b9cfab3b56b3150952a16e1
jaheinz <jaheinz@CE27823.corp.capgemini.com>
2019-06-04 09:11:59 +0200

    changes made

--- Hello World/src/de/capgemini/HelloWorld.java
+++ Hello World/src/de/capgemini/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello" + " " + "World!");
+		
+		int a = 0;
+		int b = 10;
+		
+		for (; a<b; a++) {
+			System.out.println(a);
+		}
 	}
 
 }
